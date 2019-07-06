c7f2f485a859b22f50a104665574e6f7ff997366
aedit <udisen97@gmail.com>
2019-07-06 22:32:38 +0530

    Project holder to start

---
+++ projects.js
@@ -0,0 +1,50 @@
+const projects = [
+  {
+    num: 1,
+    name: 'Udit Sen',
+    email: 'udisen97@gmail.com',
+    contact: 9826942288,
+    profile: 'https://github.com/aedit',
+    projectname: 'GDG Cloud Indore ProjectHolder',
+    projectrepo: 'https://github.com/gdg-cloud-indore/gdgcloudproject',
+    projectstack: ['React', 'JavaScript'],
+    projectabstract:
+      'A website to showcase all the projects being worked upon under GDG Cloud Indore',
+  },
+  {
+    num: 2,
+    name: 'Udit Sen',
+    email: 'udisen97@gmail.com',
+    contact: 9826942288,
+    profile: 'https://github.com/aedit',
+    projectname: 'GDG Cloud Indore ProjectHolder',
+    projectrepo: 'https://github.com/gdg-cloud-indore/gdgcloudproject',
+    projectstack: ['React', 'JavaScript'],
+    projectabstract:
+      'A website to showcase all the projects being worked upon under GDG Cloud Indore',
+  },
+  {
+    num: 3,
+    name: 'Udit Sen',
+    email: 'udisen97@gmail.com',
+    contact: 9826942288,
+    profile: 'https://github.com/aedit',
+    projectname: 'GDG Cloud Indore ProjectHolder',
+    projectrepo: 'https://github.com/gdg-cloud-indore/gdgcloudproject',
+    projectstack: ['React', 'JavaScript'],
+    projectabstract:
+      'A website to showcase all the projects being worked upon under GDG Cloud Indore',
+  },
+  {
+    num: 4,
+    name: 'Udit Sen',
+    email: 'udisen97@gmail.com',
+    contact: 9826942288,
+    profile: 'https://github.com/aedit',
+    projectname: 'GDG Cloud Indore ProjectHolder',
+    projectrepo: 'https://github.com/gdg-cloud-indore/gdgcloudproject',
+    projectstack: ['React', 'JavaScript'],
+    projectabstract:
+      'A website to showcase all the projects being worked upon under GDG Cloud Indore',
+  },
+]
